cf1312d105ef983d6089aa51796896763ac6708b
WINDOWS10 <919575700@qq.com>
2016-11-13 22:31:01 +0800

    无损压缩解压bug待解决；

--- VPT - Template/App/AppCompress.cpp
+++ VPT - Template/App/AppCompress.cpp
@@ -28,175 +28,158 @@ void CAppCompress::CustomFinal(void) {
 // The input reference variable cDataSize, is also serve as an output variable to indicate the size (in bytes) of the compressed data.
 unsigned char *CAppCompress::Compress(int &cDataSize) {
 
-	// 压缩后的数据
 	unsigned char *compressedData ;
-	// 最大长度，如果compressedSize>=cDataSize,压缩失败
 	cDataSize = width * height * 3 ;	
-	// 存储压缩后的数据					
-	compressedData = new unsigned char[cDataSize] ; 
 
 	/****************************************** My Cdoe **********************************************************/
-	int count = 0;
+
+	// 存储压缩后的数据,最差的情况尺寸也不会到大于cDataSize * 2					
+	compressedData = new unsigned char[cDataSize * 2];
 	// 实际压缩字符长度
 	int compressedSize = 0;
+
 	// 采用分通道游离的方法，按照每个通道相邻像素的重复性进行压缩
-	// b通道
+	// 1.b通道
 	unsigned short curB = pInput[0];// 第一个像素的b
 	unsigned short repeat = 1;// 重复次数
-	for (int i = 1; i < cDataSize/3; i++) {
+	for (int i = 1; i < cDataSize / 3; i++)
+	{
 		unsigned short nextB = pInput[i * 3 + 0];// 下一个像素的b
-		if (nextB == curB) {
+		if (nextB == curB && repeat < 127)
+		{
 			++repeat;
 			// 如果是最后一个则存储
-			if (i == cDataSize/3 - 1)
+			if (i == cDataSize / 3 - 1)
 			{
 				// 存储最后一个b值组
-				compressedData[compressedSize] = repeat & 0xFF;
-				compressedData[compressedSize + 1] = curB & 0xFF;
+				compressedData[compressedSize] = repeat;
+				compressedData[compressedSize + 1] = curB;
 				// 增加编码数据长度
 				compressedSize += 2;
 			}
-
-		}else{
-
+		}
+		else
+		{
 			// 存储上一个b值组
-			compressedData[compressedSize] = repeat & 0xFF;
-			compressedData[compressedSize + 1] = curB & 0xFF;
+			compressedData[compressedSize] = repeat;
+			compressedData[compressedSize + 1] = curB;
 			// 增加编码数据长度
 			compressedSize += 2;
-
 			// 换下一种b值
 			curB = nextB;
 			repeat = 1;
-
 			// 如果是最后一个
-			if (i == cDataSize/3 - 1)
+			if (i == cDataSize / 3 - 1)
 			{
 				// 存储最后一个b值
-				compressedData[compressedSize] = 1 & 0xFF;
-				compressedData[compressedSize + 1] = curB & 0xFF;
+				compressedData[compressedSize] = 1;
+				compressedData[compressedSize + 1] = curB;
 				// 增加编码数据长度
 				compressedSize += 2;
 			}
-		}
-
-		if (compressedSize > cDataSize)
-		{
-			// 压缩失败
-			memcpy(compressedData, pInput, cDataSize);
-			return compressedData;
 		}
 	}
 
-
-	// g通道
+	// 2.g通道
 	unsigned short curG = pInput[1];// 第一个像素的g
 	repeat = 1;// 重复次数
-	for (int i = 1; i < cDataSize/3; i++) {
+	for (int i = 1; i < cDataSize / 3; i++)
+	{
 		unsigned short nextG = pInput[i * 3 + 1];// 下一个像素的g
-		if (nextG == curG) {
+		if (nextG == curG && repeat <= 127)
+		{
 			++repeat;
-
 			// 如果是最后一个则存储
-			if (i == cDataSize/3 - 1)
+			if (i == cDataSize / 3 - 1)
 			{
 				// 存储最后一个g值组
-				compressedData[compressedSize] = repeat & 0xFF;
-				compressedData[compressedSize + 1] = curG & 0xFF;
+				compressedData[compressedSize] = repeat;
+				compressedData[compressedSize + 1] = curG;
 				// 增加编码数据长度
 				compressedSize += 2;
 			}
 		}
-		else{
-
+		else
+		{
 			// 存储上一个g值组
-			compressedData[compressedSize] = repeat & 0xFF;
-			compressedData[compressedSize + 1] = curG & 0xFF;
+			compressedData[compressedSize] = repeat;
+			compressedData[compressedSize + 1] = curG;
 			// 增加编码数据长度
 			compressedSize += 2;
-
 			// 换下一种g值
 			curG = nextG;
 			repeat = 1;
-
 			// 如果是最后一个
-			if (i == cDataSize/3 - 1)
+			if (i == cDataSize / 3 - 1)
 			{
 				// 存储最后一个g值
-				compressedData[compressedSize] = 1 & 0xFF;
-				compressedData[compressedSize + 1] = curB & 0xFF;
+				compressedData[compressedSize] = 1;
+				compressedData[compressedSize + 1] = curB;
 				// 增加编码数据长度
 				compressedSize += 2;
 			}
-		}
-
-		if (compressedSize > cDataSize)
-		{
-			// 压缩失败
-			memcpy(compressedData, pInput, cDataSize);
-			return compressedData;
 		}
 	}
 
-	// r通道
+	// 3.r通道
 	unsigned short curR = pInput[2];// 第一个像素的r
 	repeat = 1;// 重复次数
-	for (int i = 1; i < cDataSize/3; i++) {
+	for (int i = 1; i < cDataSize / 3; i++)
+	{
 		unsigned short nextR = pInput[i * 3 + 2];// 下一个像素的r
-		if (nextR == curR) {
+		if (nextR == curR && repeat <= 127)
+		{
 			++repeat;
-
 			// 如果是最后一个则存储
-			if (i == cDataSize/3 - 1)
+			if (i == cDataSize / 3 - 1)
 			{
 				// 存储最后一个g值组
-				compressedData[compressedSize] = repeat & 0xFF;
-				compressedData[compressedSize + 1] = curR & 0xFF;
+				compressedData[compressedSize] = repeat;
+				compressedData[compressedSize + 1] = curR;
 				// 增加编码数据长度
 				compressedSize += 2;
 			}
-
 		}
-		else{
-
+		else
+		{
 			// 存储上一个g值组
-			compressedData[compressedSize] = repeat & 0xFF;
-			compressedData[compressedSize + 1] = curR & 0xFF;
+			compressedData[compressedSize] = repeat;
+			compressedData[compressedSize + 1] = curR;
 			// 增加编码数据长度
 			compressedSize += 2;
-
 			// 换下一种r值
 			curR = nextR;
 			repeat = 1;
-
 			// 如果是最后一个
-			if (i == cDataSize/3 - 1)
+			if (i == cDataSize / 3 - 1)
 			{
 				// 存储最后一个r值
-				compressedData[compressedSize] = 1 & 0xFF;
-				compressedData[compressedSize + 1] = curR & 0xFF;
+				compressedData[compressedSize] = 1;
+				compressedData[compressedSize + 1] = curR;
 				// 增加编码数据长度
 				compressedSize += 2;
 			}
-		}
-
-		if (compressedSize > cDataSize)
-		{
-			// 压缩失败
-			memcpy(compressedData, pInput, cDataSize);
-			return compressedData;
 		}
 	}
 
 	// 取出压缩后的纯数据
 	cDataSize = compressedSize;
 	unsigned char *finalData = new unsigned char[cDataSize];
-	for (int i = 0; i < cDataSize; i++){
+	for (int i = 0; i < cDataSize; i++)
+	{
 		unsigned char temp = compressedData[i];
 		finalData[i] = temp;
 	}
-	//delete compressedData;
-	//compressedData = finalData;
+	delete compressedData;
+	compressedData = finalData;
+
+	// 调试用：
+	int pixelCount = 0;
+	for (int i = 0; i < cDataSize; i += 2)
+	{
+		int curRpeat = compressedData[i];
+		pixelCount += curRpeat;
+	}
 
 	/******************************************** My Code End ****************************************************/
 
@@ -213,66 +196,61 @@ void CAppCompress::Decompress(unsigned char *compressedData, int cDataSize, unsi
 	/******************************************** My Code *********************************************************/
 	
 	// 寻找g通道和r通道在压缩数据数组中的偏移坐标
-	int offset_r, offset_g;
-	offset_r = offset_g = 0;
+	int offset_r = 0, offset_g = 0;
 	int pixelCount = 0;
-	for (int i = 0; i < cDataSize; )
+	for (int i = 0; i < cDataSize;)
 	{
-		if (pixelCount == 189366)
+		int curRpeat = compressedData[i];
+		pixelCount += curRpeat;
+		i += 2;
+		if (pixelCount == width*height)
 		{
 			offset_g = i;// g通道的开始坐标
 		}
-		if (pixelCount == width*height*2)
+		if (pixelCount == width*height * 2)
 		{
 			offset_r = i;// r通道的开始坐标
 		}
-		pixelCount += compressedData[i];
-
-		if (pixelCount>189312*2)
-		{
-			offset_r = 0;
-		}
-
-		i += 2;
 	}
-
 
 	unsigned int b, g, r;
 	int repeat;
-	// 还原b通道
+	// 1.还原b通道
 	for (int i = 0, j = 0; i < width*height, j < offset_g; j += 2)
 	{
 		// 恢复一组重复的b值
 		repeat = compressedData[j];
 		for (int p = 0; p < repeat; p++)
 		{
-			uncompressedData[i * 3+0] = compressedData[j + 1];
+			int d = compressedData[j + 1];
+			uncompressedData[i * 3 + p*3 + 0] = compressedData[j + 1];
 		}
 		i += repeat;
 	}
 
-	// 还原g通道
-	for (int i = 0, j = offset_g; i < width*height, j<offset_r;j+=2)
+	// 2.还原g通道
+	for (int i = 0, j = offset_g; i < width*height, j < offset_r; j += 2)
 	{
 		repeat = compressedData[j];
 		for (int p = 0; p < repeat; p++)
 		{
-			uncompressedData[i * 3 + 1] = compressedData[j + 1];
+			int d = compressedData[j + 1];
+			uncompressedData[i * 3 + p * 3 + 1] = compressedData[j + 1];
 		}
 		i += repeat;
 	}
 
 	// 还原r通道
-	for (int i = 0,j=offset_r; i < width*height,j<cDataSize; j+=2)
+	for (int i = 0, j = offset_r; i < width*height, j < cDataSize; j += 2)
 	{
 		repeat = compressedData[j];
 		for (int p = 0; p < repeat; p++)
 		{
-			uncompressedData[i * 3 + 2] = compressedData[j + 1];
+			int d = compressedData[j + 1];
+			uncompressedData[i * 3 + p * 3 + 2] = compressedData[j + 1];
 		}
 		i += repeat;
 	}
-
 	/******************************************** My Code End *********************************************************/
 }
 
